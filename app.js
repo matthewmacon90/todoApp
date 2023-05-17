aa526dccd740af5bbe718ce3d64dc24679c553e4
Matthew Macon <matthewmacon90@gmail.com>
2023-05-17 15:37:48 -0400

    updated

--- app.js
+++ app.js
@@ -1,35 +1,17 @@
 const form = document.querySelector('#taskCreator');
 const input = document.querySelector('#createTask');
 const taskList = document.querySelector('#list');
-const taskCollection = [];
 
 const addToLocalStorage = (inputValue) => {
-    let taskID = `todo${taskCollection.length}`;
-    localStorage.setItem(taskID, inputValue);
-
-    let getInfo = localStorage.getItem(taskID);
-    taskCollection.push(getInfo);
-    console.log('getInfo: ', getInfo);
-    console.log(`taskCollection: `, taskCollection);
+    let key = `todo${localStorage.length}`;
+    localStorage.setItem(key, inputValue);
+    return key;
 };
 
-
-// Using this to load the values to the DOM from local storage.
-const retrieveLocalStorage = () => {
-    let i = taskCollection.length;
-    while(i > 0) {
-        const newTask = document.createElement('li');
-        newTask.innerText = JSON.parse(taskCollection[JSON.parse(i)]);
-        taskList.appendChild(newTask);
-
-        const removeBtn = document.createElement('button');
-        removeBtn.innerText = 'Remove Task';
-        removeBtn.classList.add('removeTaskBtn');
-        newTask.appendChild(removeBtn);
-        i--;
-    };
+const removeFromLocalStorage = (event) => {
+    let taskID = event.target.getAttribute('data-taskid');
+    localStorage.removeItem(taskID);
 };
-retrieveLocalStorage();
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -37,26 +19,21 @@ form.addEventListener('submit', function (event) {
         alert('Please enter a valid task.');
         return;
     };
-    newTask();
+    const key = addToLocalStorage(input.value);
+    newTask(key, input.value);
+    input.value = '';
 });
 
-const newTask = () => {
+const newTask = (key, taskValue) => {
     const newTask = document.createElement('li');
-    newTask.innerText = input.value;
+    newTask.innerText = taskValue;
     taskList.appendChild(newTask);
 
     const removeBtn = document.createElement('button');
     removeBtn.innerText = 'Remove Task';
     removeBtn.classList.add('removeTaskBtn');
-
+    removeBtn.setAttribute('data-taskid', key);
     newTask.appendChild(removeBtn);
-
-    // taskCollection[`todo ${taskCollection.length}`] = input.value;
-    // console.log(`newTask taskCollection:`, taskCollection);
-
-    addToLocalStorage(input.value);
-    input.value = '';
-
 };
 
 taskList.addEventListener('click', function (event) {
@@ -66,12 +43,15 @@ taskList.addEventListener('click', function (event) {
 
     if (event.target.tagName === 'BUTTON') {
         event.target.parentElement.remove();
-        //Work on finding a cleaner solution...If the button name changes we are screwed here. Breaks the code.
-        // let removeText = event.target.parentElement.innerText.replace('Remove Task', '');
-
-        // removeFromLocalStorage();
+        removeFromLocalStorage(event);
     };
 });
 
-//I used this code to render the todo list on my app. 
-// https://stackoverflow.com/questions/62235770/how-do-i-save-to-local-storage-via-vanilla-js
+const retrieveLocalStorage = () => {
+    const keys = Object.keys(localStorage);
+
+    keys.forEach(key => {
+        newTask(key, localStorage[key]);
+    });
+};
+retrieveLocalStorage();
